172d695da4d74504cdb39ea8961da4f9d02cb32d
ZubaerAhmed17 <zubaer-ahmed@hotmail.com>
2022-01-21 19:10:53 +0000

    final

--- script.js
+++ script.js
@@ -47,21 +47,22 @@ function moveHorses(){
 
 
         if (HorsesX[i]> 90){
-            displayBet(i)
-            alert(`${colours[i]} won the race`)
+            displayBet(bet)
+            console.log(colours[j])
+            alert(`${colours[j]} won the race`)
             resetRace()
             placeBet()
-let i=0
-while(i<numHorse){
+let j=0
+while(j<numHorse){
     // create a horse
     // document.elementById("horse0")
-    horseDiv[i] = document.createElement("div")
-    document.body.appendChild(horseDiv[i])
-    horseDiv[i].classList.add("horses")
-    horseDiv[i].style.top=i * 8 +"%"
-    horseDiv[i].style.backgroundColor=colours[i]
-    HorsesX[i]=0
-    i++
+    horseDiv[j] = document.createElement("div")
+    document.body.appendChild(horseDiv[j])
+    horseDiv[j].classList.add("horses")
+    horseDiv[j].style.top=j * 8 +"%"
+    horseDiv[j].style.backgroundColor=colours[j]
+    HorsesX[j]=0
+    j++
 }
 placeBet()
 function placeBet(){
@@ -99,14 +100,14 @@ function moveHorses(){
     requestAnimationFrame(moveHorses)
 }
 
-function displayBet(i){
-    if (bet != colours[i]){
-        alert("you lost the bet")
-    }
-    else{
-        alert("you won the bet")
-    }
-}
+// function displayBet(i){
+//     if (bet != colours[i]){
+//         alert("you lost the bet")
+//     }
+//     else{
+//         alert("you won the bet")
+//     }
+// }
 
         }
         i++
